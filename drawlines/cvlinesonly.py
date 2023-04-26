7ddbd746bdee9409a9cfd6859060bbed5e171fe5
CopilotCoding <105129338+CopilotCoding@users.noreply.github.com>
2023-04-26 14:39:49 -0700

    Create cvlinesonly.py

---
+++ drawlines/cvlinesonly.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import pyautogui
+import time
+
+# Wait for some time for the user to switch to the desired window, waits for 10 seconds by default
+time.sleep(10)
+# Take a screenshot
+screenshot = pyautogui.screenshot()
+# Convert the screenshot to a NumPy array
+screenshot = np.array(screenshot)
+# Convert the screenshot to grayscale
+gray = cv2.cvtColor(screenshot, cv2.COLOR_RGB2GRAY)
+# Apply histogram equalization
+equ = cv2.equalizeHist(gray)
+# Apply Canny edge detection with adjusted parameters
+edges = cv2.Canny(equ, 50, 150, apertureSize=3)
+# Find contours
+contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Create a blank image of the same size as the original screenshot
+red_lines = np.zeros_like(screenshot)
+# Draw red lines around the edges of the screenshot on the blank image
+cv2.drawContours(red_lines, contours, -1, (0, 0, 255), 2)
+# Display the new image with red lines around the edges
+cv2.imshow('Red Lines', red_lines)
+# Wait for user input
+key = cv2.waitKey(0)
+# Exit the program
+cv2.destroyAllWindows()
